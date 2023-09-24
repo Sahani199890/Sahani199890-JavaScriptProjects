1a35fbfee590b4a68edbb0d74c1fe7ec1fa795f2
Abhishek Sahani <sahani199890@gmail.com>
2023-09-24 22:56:22 +0530

    small changes in css

--- GuessTheNumber.js
+++ GuessTheNumber.js
@@ -3,42 +3,57 @@
 let count = 20;
 let maxScore = 0;
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
-let message = document.querySelector(".message");
-let number = document.querySelector(".number");
-let highScore = document.querySelector(".highscore");
-let guess = document.querySelector(".guess");
-let again = document.querySelector(".again");
+const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+const highScore = document.querySelector(".highscore");
+const guess = document.querySelector(".guess");
+const again = document.querySelector(".again");
+const score = document.querySelector(".score");
+const body = document.querySelector("body");
+
 again.addEventListener("click", () => {
   message.textContent = "Start guessing";
   guess.value = null;
   number.textContent = "?";
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  body.style.backgroundColor = "#303030";
+  number.style.width = "15rem";
   count = 20;
 });
 
 const handleGuessNumberFunction = () => {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-  if (!guess) {
-    message.textContent = "⛔ No Number...";
-    count--;
-  } else if (randomNumber > guess) {
-    message.textContent = "Your guess is too less";
-    count--;
-  } else if (randomNumber < guess) {
-    message.textContent = "Your guess is too high";
-    count--;
-  } else if (randomNumber === guess) {
-    count--;
-    message.textContent = "🎉Correct Number!";
-    number.textContent = count;
-    if (maxScore < count) {
-      maxScore = count;
-      highScore.textContent = maxScore;
+  if (count > 0) {
+    if (!guess) {
+      message.textContent = "⛔ No Number...";
+      count--;
+      score.textContent = count;
+    } else if (randomNumber > guess) {
+      message.textContent = "Your guess is too less";
+      count--;
+      score.textContent = count;
+    } else if (randomNumber < guess) {
+      message.textContent = "Your guess is too high";
+      count--;
+      score.textContent = count;
+    } else if (randomNumber === guess) {
+      count--;
+      message.textContent = "🎉Correct Number!";
+      number.textContent = randomNumber;
+      number.style.width = "30rem";
+      score.textContent = count;
+      body.style.backgroundColor = "green";
+
+      if (maxScore < count) {
+        maxScore = count;
+        highScore.textContent = maxScore;
+      }
+    } else if (count < 5) {
+      message.textContent = "You Lose🥱";
     }
-  } else if (count < 0) {
-    message.textContent = "You Lose🥱";
   } else {
-    message.textContent = "Start guessing...";
+    body.style.backgroundColor = "red";
   }
 };
 
